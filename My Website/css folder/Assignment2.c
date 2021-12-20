7768b749aeab7a0542ae8a6b2f192ef8a817ad5f
Sean Davies <64815512+davies02@users.noreply.github.com>
2021-12-20 16:03:12 -0500

    Delete Assignment2.c

--- My Website/css folder/Assignment2.c
+++
@@ -1,9 +0,0 @@
-#include "givenA2.h"
-
-void createInitialBoard(char board [N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("?");
-        }
-    }
-}
