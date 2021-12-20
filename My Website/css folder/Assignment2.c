2ce82ac3273f530f87a1b91778559a0e85896470
Sean Davies <64815512+davies02@users.noreply.github.com>
2021-12-20 16:01:38 -0500

    Add files via upload

---
+++ My Website/css folder/Assignment2.c
@@ -0,0 +1,9 @@
+#include "givenA2.h"
+
+void createInitialBoard(char board [N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("?");
+        }
+    }
+}
